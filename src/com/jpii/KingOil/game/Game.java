5aa4d0627651deddfb575b3bf9bd380624e03e53
abauer <abauer@jpiicardinals.org>
2012-11-20 16:24:51 -0600

    added basic gamebackground

--- src/com/jpii/KingOil/game/Game.java
+++ src/com/jpii/KingOil/game/Game.java
@@ -1,12 +1,39 @@
 package com.jpii.KingOil.game;
 
-import com.jpii.KingOil.gui.*;
+import com.jpii.KingOil.gui.Window;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.imageio.ImageIO;
 
 @SuppressWarnings("serial")
 public class Game extends Window {
 	public Game() {
-		super(800,800);
+		super(780,750);
+		init();
 		setDefaults();
+	}
+	
+	BufferedImage gameboard;
+	Image buffer;
+	Graphics g;
+	
+	public void init(){
+		try{
+			gameboard = ImageIO.read(Game.class.getResource("/com/jpii/KingOil/res/GameBoard.png"));
+		}catch(Exception e) {e.printStackTrace();}
+		
+		BufferedImage img = new BufferedImage(width+16, height+38, BufferedImage.TYPE_INT_ARGB);
+		buffer = img;
+		g = buffer.getGraphics();
+	}
+	
+	public void paint(Graphics paint){
+		g.setColor(Color.black);
+		g.fillRect(0,0,780,750);
+		g.drawImage(gameboard, 0,0,this);
+		paint.drawImage(buffer,0,0,this);
 	}
 	
 }
