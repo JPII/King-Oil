686dd28ba41d1969939873533858feadc8e8c7a1
abauer <abauer@jpiicardinals.org>
2012-11-20 16:50:28 -0600

    added center function on doubleclick

--- src/com/jpii/KingOil/game/Game.java
+++ src/com/jpii/KingOil/game/Game.java
@@ -1,5 +1,6 @@
 package com.jpii.KingOil.game;
 
+import com.jpii.KingOil.KingOil;
 import com.jpii.KingOil.gui.Window;
 
 import java.awt.*;
@@ -18,8 +19,11 @@ public class Game extends Window {
 	BufferedImage gameboard;
 	Image buffer;
 	Graphics g;
+	int boardx,boardy;
 	
-	public void init(){
+	boolean pressed = false;
+	
+	private void init(){
 		try{
 			gameboard = ImageIO.read(Game.class.getResource("/com/jpii/KingOil/res/GameBoard.png"));
 		}catch(Exception e) {e.printStackTrace();}
@@ -31,6 +35,7 @@ public class Game extends Window {
 		public void mouseMoved(MouseEvent md){mouseM(md);}};
 		this.addMouseMotionListener(mouse1);
 		
+		initializePoints();
 		
 		BufferedImage img = new BufferedImage(width+16, height+38, BufferedImage.TYPE_INT_ARGB);
 		buffer = img;
@@ -40,21 +45,39 @@ public class Game extends Window {
 	public void paint(Graphics paint){
 		g.setColor(Color.black);
 		g.fillRect(0,0,780,750);
-		g.drawImage(gameboard, 0,0,this);
+		
+		
+		
+		g.drawImage(gameboard, 0+boardx,0+boardy,this);
 		paint.drawImage(buffer,0,8,this);
 	}
 	
-	public void mousePressed2(MouseEvent e) {
+	/////  Mouse Events  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private void mousePressed2(MouseEvent e) {
+		if (e.getClickCount() == 2)
+			centerScreen(e.getX(),e.getY());
     }
 
-	public void mouseDrag(MouseEvent e) {
+	private void mouseDrag(MouseEvent e) {
     }
 
-    public void mouseM(MouseEvent e) {
+    private void mouseM(MouseEvent e) {
     	
     }
 
-    public void mouseReleased2(MouseEvent e) {
+    private void mouseReleased2(MouseEvent e) {
 	}
-	
+    
+	/////  Other methods  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void initializePoints(){
+    	
+    }
+    
+    private void centerScreen(int x, int y){
+    	boardx-=(x-width/2);
+    	boardy-=((y-height/2)-8);
+    	repaint();
+    }
 }
