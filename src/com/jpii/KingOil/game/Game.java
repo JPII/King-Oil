9a4381d4ce8bb57a53e483784e1ec8463a5600f2
abauer <abauer@jpiicardinals.org>
2012-11-20 16:09:16 -0600

    fixed some images problems and added gamebored

--- src/com/jpii/KingOil/game/Game.java
+++ src/com/jpii/KingOil/game/Game.java
@@ -3,6 +3,11 @@ package com.jpii.KingOil.game;
 import com.jpii.KingOil.gui.*;
 
 @SuppressWarnings("serial")
-public class Game extends Window{
-
+public class Game extends Window {
+	public Game() {
+		super(800,800);
+		
+		setDefaults();
+	}
+	
 }
